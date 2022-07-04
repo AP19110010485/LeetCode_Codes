6f037f9c5b0c1d20fd95feb4e26550cae2a91e75
Souvik Mandal <62840173+AP19110010485@users.noreply.github.com>
2022-07-05 00:05:26 +0530

    Update 376-wiggle-subsequence.py

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -4,7 +4,7 @@ class Solution:
         
         if not dp:
             return 1
-        
+        # If an array has at least two numbers and those elements aren't the same number, the minimum subsequence will be at least 2.
         count=2
         for i in range(1,len(dp)):
             if dp[i-1]>0 and dp[i]<0 or dp[i-1]<0 and dp[i]>0:
